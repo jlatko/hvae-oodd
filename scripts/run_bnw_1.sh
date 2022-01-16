c1f8302c319cbfebd5cb376fcc46d426a5e38dfb
Janek <jan.piotr.latko@gmail.com>
2022-01-16 13:10:34 +0100

    more experiments

--- scripts/run_bnw_1.sh
+++ scripts/run_bnw_1.sh
@@ -1,19 +1,26 @@
-GPU=2
-#LIKELIHOOD="DiscretizedLogisticMixLikelihoodConv2dMono"
-LIKELIHOOD="GaussianLikelihoodConv2d"
-
-VAL_DATA="scripts/configs/val_datasets/bnw_dequantized.json"
+GPU=5
+LIKELIHOOD="BernoulliLikelihoodConv2d"
+VAL_DATA="scripts/configs/val_datasets/binarized.json"
 
 echo "Running experiments on GPU $GPU"
 
+#         'FashionMNISTBinarized': {'split': 'validation', 'dynamic': False},
+#        'MNISTBinarized': {'split': 'validation', 'dynamic': False},
+#        'notMNISTBinarized': {'split': 'validation'},
+#        'Omniglot28x28Binarized': {'split': 'validation'},
+#        'Omniglot28x28InvertedBinarized': {'split': 'validation'},
+# not doing small norb as it's bad binarized
+#        'SmallNORB28x28Binarized': {'split': 'validation'},
+#        'KMNISTBinarized': {'split': 'validation', 'dynamic': False}
+
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --seed 1 \
-                               --run_name=gaussian_05_nats \
+                               --run_name=05_nats \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "FashionMNISTDequantized": {"dynamic": true, "split": "train"}
+                                   "FashionMNISTBinarized": {"dynamic": true, "split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
@@ -23,11 +30,11 @@ CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --seed 1 \
-                               --run_name=gaussian_05_nats \
+                               --run_name=05_nats \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "MNISTDequantized": {"dynamic": true, "split": "train"}
+                                   "MNISTBinarized": {"dynamic": true, "split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
@@ -37,26 +44,11 @@ CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --seed 1 \
-                               --run_name=gaussian_05_nats \
+                               --run_name=05_nats \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "KMNISTDequantized": {"dynamic": true, "split": "train"}
-                               }' \
-                               --val_datasets $VAL_DATA \
-                               --likelihood $LIKELIHOOD \
-                               --config_deterministic scripts/configs/default_model/config_deterministic_bnw.json \
-                               --config_stochastic scripts/configs/default_model/config_stochastic_bnw.json
-
-
-
-CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
-                               --seed 1 \
-                               --run_name=gaussian_05_nats \
-                               --free_nats_end 0.5 \
-                               --train_datasets \
-                               '{
-                                   "notMNISTDequentized": {"dynamic": true, "split": "train"}
+                                   "KMNISTBinarized": {"dynamic": true, "split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
@@ -66,11 +58,11 @@ CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --seed 1 \
-                               --run_name=gaussian_05_nats \
+                               --run_name=05_nats \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "Omniglot28x28Dequentized": {"dynamic": true, "split": "train"}
+                                   "notMNISTBinarized": {"dynamic": true, "split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
@@ -79,11 +71,24 @@ CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --seed 1 \
-                               --run_name=gaussian_05_nats \
+                               --run_name=05_nats \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "Omniglot28x28InvertedDequentized": {"dynamic": true, "split": "train"}
+                                   "Omniglot28x28Binarized": {"dynamic": true, "split": "train"}
+                               }' \
+                               --val_datasets $VAL_DATA \
+                               --likelihood $LIKELIHOOD \
+                               --config_deterministic scripts/configs/default_model/config_deterministic_bnw.json \
+                               --config_stochastic scripts/configs/default_model/config_stochastic_bnw.json
+
+CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
+                               --seed 1 \
+                               --run_name=05_nats \
+                               --free_nats_end 0.5 \
+                               --train_datasets \
+                               '{
+                                   "Omniglot28x28InvertedBinarized": {"dynamic": true, "split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
@@ -93,11 +98,11 @@ CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --seed 1 \
-                               --run_name=gaussian_05_nats \
+                               --run_name=05_nats \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "SmallNORB28x28Dequentized": {"split": "train"}
+                                   "SmallNORB28x28Binarized": {"split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
