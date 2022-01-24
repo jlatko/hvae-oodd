974e4a43982ce1d3a2886fdb43314963ba31acb3
Janek <jan.piotr.latko@gmail.com>
2022-01-24 13:03:17 +0100

    experiments

---
+++ scripts/run_results.sh
@@ -0,0 +1,4 @@
+env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=dsg0jhjx
+env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=2oyfdrgk
+env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=3cuq3l6l
+env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=1om1eeyz
