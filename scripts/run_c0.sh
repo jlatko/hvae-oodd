890cb27a8d1bca3837f043f6a3c6c69b136ec13e
Janek <jan.piotr.latko@gmail.com>
2022-01-24 10:37:37 +0100

    experiments

--- scripts/run_c0.sh
+++ scripts/run_c0.sh
@@ -7,9 +7,9 @@ VAL_DATA="scripts/configs/val_datasets/32_dequantized.json"
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --special \
                                --seed 1 \
-                               --run_name=1000_1_beta_2_nats_05 \
-                               --epochs 1000 \
-                               --max_beta 2 \
+                               --run_name=2000_1_nats_05 \
+                               --epochs 2000 \
+                               --max_beta 1 \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
@@ -20,21 +20,39 @@ CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --config_deterministic scripts/configs/default_model/config_deterministic_32.json \
                                --config_stochastic scripts/configs/default_model/config_stochastic_32.json
 
+CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
+                               --special \
+                               --seed 1 \
+                               --run_name=2000_1_nats_05 \
+                               --epochs 2000 \
+                               --max_beta 1 \
+                               --free_nats_end 0.5 \
+                               --train_datasets \
+                               '{
+                                   "SVHNDequantized": {"dynamic": true, "split": "train"}
+                               }' \
+                               --val_datasets $VAL_DATA \
+                               --likelihood $LIKELIHOOD \
+                               --config_deterministic scripts/configs/default_model/config_deterministic_32.json \
+                               --config_stochastic scripts/configs/default_model/config_stochastic_32.json
 
 CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
                                --special \
                                --seed 1 \
                                --run_name=1000_1_nats_05 \
                                --epochs 1000 \
+                               --max_beta 1 \
                                --free_nats_end 0.5 \
                                --train_datasets \
                                '{
-                                   "FFHQ32Dequantized": { "split": "train"}
+                                   "CIFAR10Dequantized": {"dynamic": true, "split": "train"}
                                }' \
                                --val_datasets $VAL_DATA \
                                --likelihood $LIKELIHOOD \
                                --config_deterministic scripts/configs/default_model/config_deterministic_32.json \
                                --config_stochastic scripts/configs/default_model/config_stochastic_32.json
+
+
 
 #CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
 #                               --special \
