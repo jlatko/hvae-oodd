3008a6a7ac95b4529fa022e3bbbf628d060ab4bf
Janek <jan.piotr.latko@gmail.com>
2022-02-14 12:22:23 +0100

    resample

--- scripts/run_resampled_stats.sh
+++ scripts/run_resampled_stats.sh
@@ -13,11 +13,15 @@
 #                                  --use_train --val_datasets='{"CIFAR10Dequantized": {"dynamic": false, "split": "validation"}}'
 
 CUDA_VISIBLE_DEVICES=0 python scripts/ood-llr.py --run_ids=2i3lx19p --run_name=fmnist_pow_low \
-                                  --use_train --val_datasets='{"FashionMNISTBinarized": {"split": "validation"}}'
+                                  --use_train --val_datasets='{"FashionMNISTBinarized": {"split": "validation"}}' \
+                                  --n_eval_examples 100000
 CUDA_VISIBLE_DEVICES=0 python scripts/ood-llr.py --run_ids=1y6myw1d --run_name=fmnist_pow_hi \
-                                  --use_train --val_datasets='{"FashionMNISTBinarized": {"split": "validation"}}'
+                                  --use_train --val_datasets='{"FashionMNISTBinarized": {"split": "validation"}}' \
+                                  --n_eval_examples 100000
 
 CUDA_VISIBLE_DEVICES=0 python scripts/ood-llr.py --run_ids=1la9248j --run_name=cifar_pow_low \
-                                  --use_train --val_datasets='{"CIFAR10Dequantized": {"dynamic": false, "split": "validation"}}'
+                                  --use_train --val_datasets='{"CIFAR10Dequantized": {"dynamic": false, "split": "validation"}}' \
+                                  --n_eval_examples 100000
 CUDA_VISIBLE_DEVICES=0 python scripts/ood-llr.py --run_ids=22snv7ne --run_name=cifar_pow_hi \
-                                  --use_train --val_datasets='{"CIFAR10Dequantized": {"dynamic": false, "split": "validation"}}'
+                                  --use_train --val_datasets='{"CIFAR10Dequantized": {"dynamic": false, "split": "validation"}}' \
+                                  --n_eval_examples 100000
