714472e35618d538f353bc9c210b91688568d9a7
Janek <jan.piotr.latko@gmail.com>
2022-04-18 11:14:05 +0200

    refactor

--- scripts/run_results.sh
+++
@@ -1,4 +0,0 @@
-env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=dsg0jhjx
-env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=2oyfdrgk
-env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=3cuq3l6l
-env CUDA_VISIBLE_DEVICES='' python scripts/get_results.py --run_id=2sx9czcl
